653a8eff9cbebf24c34c0c6667c2035ba8b76ca0
eyasfar <sfar.eya17@gmail.com>
2021-12-30 15:46:14 +0100

    multer & cloudinary implementation

--- server/models/painting.js
+++ server/models/painting.js
@@ -3,13 +3,9 @@ var mongoose = require('mongoose');
 var PaintingSchema = new mongoose.Schema({
   name: String,
   artist: String,
-<<<<<<< HEAD
   year: Number,
   paintingImg: String,
   cloudinary_id: String
-=======
-  year: Number
->>>>>>> 73f99a36f75c86a1a31f8e580776282557643932
 });
 
 module.exports = mongoose.model('Painting', PaintingSchema);
