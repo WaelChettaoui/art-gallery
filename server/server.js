bdf41fee35490c9f7ba20221c4882a9f4e73a44d
eyasfar <sfar.eya17@gmail.com>
2021-12-31 10:00:15 +0100

    Fixed the PUT request to also update the artist and year

--- server/server.js
+++ server/server.js
@@ -1,40 +1,49 @@
+// Setting up Express & Middlewares
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
+var logger = require('morgan'); // HTTP Logging Middleware
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config()
-mongoose.connect('mongodb://localhost/mern-crud')
+
+// Connecting to the DB
+const uri = process.env.MONGO_URI
+
+mongoose.connect(uri)
   .then(() =>  console.log('Connected to the DB'))
   .catch((err) => console.error(err));
 
-var paintingRoute = require('./routes/painting.routes');
+// Creating the app
 var app = express();
 
+// Middlewares
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
+
+// Routes
+var paintingRoute = require('./routes/painting.routes');
 app.use('/api/v1/painting', paintingRoute);
 
-// catch 404 and forward to error handler
+// Catching 404 error and forwarding to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing the error in dev
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.send(err);
 });
 
 const PORT = process.env.port;
